d15be3abfce46b99e4d8ce0df6ec185fad352771
adolfonaranjo4 <130087568+adolfonaranjo4@users.noreply.github.com>
2023-04-06 18:01:37 -0400

    Add files via upload
    First Submission IDLE

---
+++ P1HW1_NaranjoAdolfo.py
@@ -0,0 +1,29 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/adolf/OneDrive/Documents/P1HW1_NaranjoAdolfoS.py ======
+Enter a budget please: 2000
+Travel destination: Orlando
+Gas: 400
+Accomodation: 500
+Food: 500
+This program calculates and displays travel expenses
+Enter Budget: 2000
+
+Enter your travel destination: Orlando
+
+How much do you think you will spend on gas? 400
+
+Approximately, how much will you need for accomodation/hotel? 500
+
+Last, how much do you need for food? 500
+
+----------Travel Expenses----------
+Location: Orlando
+Initial Budget: 2000
+
+Fuel: 400
+Accomodation: 500
+Food: 500
+
+Remaining Balance: 600
