13f4abbb598776080bda951c65d8694dc64a3eab
adolfonaranjo4 <130087568+adolfonaranjo4@users.noreply.github.com>
2023-04-21 17:19:38 -0400

    Add files via upload

--- P2HW2_NaranjoAdolfo.py
+++ P2HW2_NaranjoAdolfo.py
@@ -1,39 +1,23 @@
-num1 =  int( input('Enter a budget please: '))
+#CTI-110
+#P2HW2 - List
+#Adolfo Naranjo
+#04-21-2023
 
-travel_destination = input('Travel destination: ')
+grade_module1 = float(input('Enter grade for Module 1: '))
+grade_module2 = float(input('Enter grade for Module 2: '))
+grade_module3 = float(input('Enter grade for Module 3: '))
+grade_module4 = float(input('Enter grade for Module 4: '))
+grade_module5 = float(input('Enter grade for Module 5: '))
+grade_module6 = float(input('Enter grade for Module 6: '))
 
-num2 = int( input('Gas: '))
-
-num3 = int( input('Accomodation: '))
-
-num4 = int( input('Food: '))
-
-expenses = num2 + num3 + num4
-
-remaining_balance = num1 - expenses
-
-# This program calculates and displays travel expenses
-# 04/06/2023
-# CTI-110 P1HW1-Travel 
-# Adolfo Naranjo
+grades = [65.5, 88, 78.5, 90, 61, 92]
+average_grade = sum(grades) / len(grades)
 
 
-print('This program calculates and displays travel expenses')
-print('Enter Budget:', num1)
-print()
-print('Enter your travel destination:', travel_destination)
-print()
-print('How much do you think you will spend on gas?', num2)
-print()
-print('Approximately, how much will you need for accomodation/hotel?', num3)
-print()
-print('Last, how much do you need for food?', num4)
-print()
-print('----------Travel Expenses----------')
-print('Location:', travel_destination)
-print(f"{'Initial Budget:' : <20}", f"{ '$'+str(float(num1)) : <20}")
-print(f"{'Fuel:' : <20}", f"{'$'+str(float(num2)) : <20}")
-print(f"{'Accomodation:' : <20}", f"{'$'+str(float(num3)) : <20}")
-print(f"{'Food:' : <21}" f"{'$'+str(float(num4)) : <21}")
-print()
-print(f"{'Remaining Balance:': <21}" f"{'$'+str(float(remaining_balance)) : <21}")
+
+print('------------Results-----------')
+print(f'Lowest Grade: {min(grades)}')
+print(f'Highest Grade: {max(grades)}')
+print(f'Sum of Grades: {sum(grades)}')
+print(f'Average Grade: {average_grade}')     
+
